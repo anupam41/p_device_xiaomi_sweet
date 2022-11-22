a545c9dcc9fec95a4da0779471110767fbb67c93
anupam41 <anupamsamanta41@gmail.com>
2022-11-22 20:39:03 +0100

    sweet: Welcome PixysOS
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- pixys_sweet.mk
+++ pixys_sweet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The pixysOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common pixysOS stuff
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := pixys_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
