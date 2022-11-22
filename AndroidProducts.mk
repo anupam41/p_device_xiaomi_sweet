7e1ea5edecb8fb92706cce4a05bacd130a0c7825
anupam41 <anupamsamanta41@gmail.com>
2022-11-22 20:39:03 +0100

    sweet: Welcome PixysOS
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The pixysOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/pixys_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixys_sweet-user \
+    pixys_sweet-userdebug \
+    pixys_sweet-eng
