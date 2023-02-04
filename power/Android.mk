Klozz/android_device_xiaomi_sweet

sweet: power: Handle launch and interaction hints for sm6150
Author: BeYkeRYkt <beykerykt@gmail.com>
Date:   Thu Dec 13 06:42:35 2018 +0900

    power: Handle launch and interaction hints for perf HAL platforms

     * MP-CTL does not handle POWER_HINT_INTERACTION and POWER_HINT_LAUNCH
       directly. Requests for processing are sent by Qualcomm BoostFramework
       (QPerformance) at the framework service level. Since we do not have
       BoostFramework, process POWER_HINT_INTERACTION and POWER_HINT_LAUNCH
       in PowerHAL to ensure a sufficient level of performance.
     * For proper operation, perfboostsconfig.xml file is required.

    Change-Id: I4c67d886c9dd74ae07094d32bdffc8ef403e04e7

Author: tomascus <arbiter1000@gmail.com>
Date:   Wed Feb 13 14:42:27 2019 +1100

    power: don't use SCROLL_PREFILING

    The current use of the SCROLL_PREFILING hint hurts scroll performance
    and results in a lot of dropped frames.

    The hint ID corresponding to SCROLL_PREFILING only takes into effect for
    a default timeout value of 80 ms (as found in perfboostsconfig.xml),
    while the duration value in the process_interaction_hint function runs
    for a much longer time, from 1500 ms up to 5000 ms (bounds of
    kMinFlingDuration and kMaxInteractiveDuration). During the period
    of time, when the timeout value has elapsed and current time is less
    than duration, another interaction hint cannot be processed. Therefore,
    if the user decides to scroll during this period, high frame drops can be
    experienced as the CPU freq isn't raised by processing another hint to
    account for this additional scrolling.
    By only using SCROLL_VERTICAL, fluidity can be maintained, improving user
    experience.

    Furthmore, kMinInteractiveDuration has been lowered to 400 ms for some
    platforms as it is more than sufficient to maintain fluidity (minimal
    frames dropped) while scrolling.

    Change-Id: I42edd07a67c7dca43a10ae2fa1b44b64b7794b43

Author: t

power/Android.mk
@@ -15,7 +15,7 @@ LOCAL_REQUIRED_MODULES := android.hardware.power-service.rc
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libbinder_ndk libutils android.hardware.power-ndk_platform vendor.xiaomi.hardware.touchfeature@1.0
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c main.cpp Power.cpp
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c main.cpp Power.cpp power-6150.c
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 

anupam41/p_device_xiaomi_sweet

power/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libutils androi
 LOCAL_SHARED_LIBRARIES += libhidlbase libhidltransport vendor.xiaomi.hardware.touchfeature@1.0
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c Power.cpp main.cpp
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c Power.cpp main.cpp power-6150.c
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 